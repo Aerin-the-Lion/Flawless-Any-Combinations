5f70a679fab56aaecf8a900a0668ce035c3bfe87
Aerin-the-Lion <skyrimriftenaerin@gmail.com>
2023-09-23 17:18:00 +0900

    Initial commit

---
+++ DisableCombination.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.UI;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+
+namespace FlawlessAnyCombinations
+{
+    internal class DisableCombination : MonoBehaviour
+    {
+        //どちらのジャンルorテーマも在り、そしてそのジャンルorテーマのコンボが正しい場合は、Trueを返す。
+        //そして、その後の処理において、IsGenreCombinationがFalseの場合は、評価値を下げるということをしている。
+        //この、評価値を下げることをDisableする。要は、どのコンボでも、たとえ一つのジャンルでも、評価値は一定になるということ。
+
+        [HarmonyPrefix, HarmonyPatch(typeof(genres), "IsGenreCombination")]
+        static bool DisableGenreCombination(genres __instance, int genre_, int subgenre_, ref bool __result)
+        {
+            if (!Main.CFG_IS_ENABLED.Value) { return true; }
+
+            if (!Main.CFG_isDisableSubGenreRequired.Value && Main.CFG_isDisableGenreCombinationRequired.Value)
+            {
+                __result = genre_ >= 0 && subgenre_ >= 0;
+                return false;
+            }
+
+            if (!Main.CFG_isDisableSubGenreRequired.Value && !Main.CFG_isDisableGenreCombinationRequired.Value)
+            {
+                __result = genre_ >= 0 && subgenre_ >= 0 && __instance.genres_COMBINATION[genre_, subgenre_];
+                return false;
+            }
+
+            __result = true;
+            return false;
+
+            //original
+            //return genre_ >= 0 && subgenre_ >= 0 && __instance.genres_COMBINATION[genre_, subgenre_];
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(themes), "IsThemesFitWithGenre")]
+        static bool DisableThemeCombination(genres __instance, int theme_, int genre_, ref bool __result)
+        {
+            if (!Main.CFG_IS_ENABLED.Value) { return true; }
+
+
+            if (!Main.CFG_isDisableMainThemeCombinationRequired.Value){ return true; }
+
+            __result = true;
+            return false;
+
+            //original
+            //return theme_ >= 0 && genre_ >= 0 && theme_ <= this.themes_LEVEL.Length && this.themes_FITGENRE[theme_, genre_];
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(genres), "IsTargetGroup")]
+        static bool DisableTargetGroupCombination(genres __instance, int genre_, int group_, ref bool __result)
+        {
+            if (!Main.CFG_IS_ENABLED.Value) { return true; }
+            if (!Main.CFG_isDisableTargetGroupCombinationRequired.Value) { return true; }
+
+            __result = true;
+            return false;
+
+            //original
+            //return genre_ >= 0 && group_ >= 0 && this.genres_TARGETGROUP[genre_, group_];
+        }
+        /*
+        //ゲームの機能の評価点を増やす関数、テストで作っただけ
+        [HarmonyPrefix, HarmonyPatch(typeof(gameplayFeatures), "GetBonus")]
+        static bool AnythingGood(gameplayFeatures __instance, int i, int maingenre_, int subgenre_, ref float __result)
+        {
+            __instance.gameplayFeatures_GOOD[0,1] = __instance.gameplayFeatures_GOOD[i, maingenre_];
+            if (__instance.gameplayFeatures_GOOD[i, maingenre_])
+            {
+                Debug.Log("Success to GetBonus!");
+                return true;
+            }
+            else
+            {
+                Debug.Log("Failed to GetBonus");
+                __result = 999f;
+                return false;
+            }
+        }
+        */
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace FlawlessAnyCombinations
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    [BepInProcess("Mad Games Tycoon 2.exe")]
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginGuid = "me.Aerin_the_Lion.Mad_Games_Tycoon_2.plugins.FlawlessAnyCombinations";
+        public const string PluginName = "FlawlessAnyCombinations";
+        public const string PluginVersion = "1.0.0.0";
+
+        public static ConfigEntry<bool> CFG_IS_ENABLED { get; private set; }
+        public static ConfigEntry<bool> CFG_isDisableGenreCombinationRequired { get; private set; }
+        public static ConfigEntry<bool> CFG_isDisableSubGenreRequired { get; private set; }
+
+        public static ConfigEntry<bool> CFG_isDisableMainThemeCombinationRequired { get; private set; }
+        public static ConfigEntry<bool> CFG_isDisableTargetGroupCombinationRequired { get; private set; }
+
+        //public static ConfigEntry<bool> CFG_isDisableSubThemeRequired { get; private set; }
+        public void LoadConfig()
+        {
+            string textIsEnable = "0. MOD Settings";
+            string genreCombination = "1. Genre Combination Settings";
+            string themesCombination = "2. Themes Combination Settings";
+            string targetGroupCombination = "3. Target Group Combination Settings";
+            CFG_IS_ENABLED = Config.Bind<bool>(textIsEnable, "Activate the MOD", true, "If you need to disable the mod, Toggle it to Disabled");
+
+            CFG_isDisableGenreCombinationRequired = Config.Bind<bool>(genreCombination, "Disable Genre Combination", true, "If you need to disable the feature of Genre Combination impacts game scores, Toggle it to Disabled");
+            CFG_isDisableSubGenreRequired = Config.Bind<bool>(genreCombination, "Disable Subgenre Impact", true, "If you need to disable the feature that Subgenre impacts game scores, toggle it to Disabled.");
+            CFG_isDisableMainThemeCombinationRequired = Config.Bind<bool>(themesCombination, "Disable Themes Combination", true, "If you need to disable the feature of Main(Sub) theme Combination impacts game scores, Toggle it to Disabled");
+            CFG_isDisableTargetGroupCombinationRequired = Config.Bind<bool>(targetGroupCombination, "Disable Target Group Combination", true, "If you need to disable the feature of Target Group Combination impacts game scores, Toggle it to Disabled");
+            //CFG_isDisableSubThemeRequired = Config.Bind<bool>(themesCombination, "Disable Sub Theme Combination", true, "If you need to disable the feature of Sub theme Combination impacts game scores, toggle it to Disabled.");
+
+            Config.SettingChanged += delegate (object sender, SettingChangedEventArgs args){};
+        }
+
+        void Awake()
+        {
+            LoadConfig();
+            Harmony.CreateAndPatchAll(typeof(DisableCombination));
+
+        }
+
+        void Update()
+        {
+            //UpdateCount++;
+            //Debug.Log("Update Count : " + UpdateCount);
+        }
+    }
+
+
+}
